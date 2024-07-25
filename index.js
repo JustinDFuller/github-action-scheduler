3074d6307858c850e93e79e230166a54c47e68ac
Justin Fuller <justindanielfuller@gmail.com>
2024-07-25 23:28:37 +0000

    test

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+const fs = require('fs').fsPromises;
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 
-try {
-    const branch = core.getInput('branch');
-    if (!branch) {
-        throw new Error(`Expected a branch to lock. Got: "${branch}"`);
+async function main() {
+    try {
+        const branch = core.getInput('branch');
+        if (!branch) {
+            throw new Error(`Expected a branch to lock. Got: "${branch}"`);
+        }
+    
+        const scheduleFilePath = core.getInput('schedule-file-path');
+        if (!scheduleFilePath) {
+            throw new Error(`Expected a schedule file path. Got: "${scheduleFilePath}`);
+        }
+    
+        core.notice(`Branch: ${branch}`);
+        core.notice(`Schedule File Path: ${scheduleFilePath}`);
+    
+        const scheduleFile = await fs.readFile(scheduleFilePath, 'utf-8')
+    
+        core.notice(`Schedule file: ${scheduleFile}`);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    const scheduleFilePath = core.getInput('schedule-file-path');
-    if (!scheduleFilePath) {
-        throw new Error(`Expected a schedule file path. Got: "${scheduleFilePath}`);
-    }
-
-    core.notice(`Branch: ${branch}`);
-    core.notice(`Schedule File Path: ${scheduleFilePath}`);
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+main();
