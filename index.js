7dfae28708bd80c03423a5f1d3dcc26b7f669895
Justin Fuller <justindanielfuller@gmail.com>
2024-07-26 12:36:22 +0000

    test

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const fs = require("fs/promises");
 const core = require("@actions/core");
-// const github = require('@actions/github');
+const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const DAYS = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
 
 async function main() {
   try {
@@ -16,12 +31,12 @@ async function main() {
       );
     }
 
-    core.debug(`Branch: ${branch}`);
-    core.debug(`Schedule File Path: ${scheduleFilePath}`);
+    core.notice(`Branch: ${branch}`);
+    core.notice(`Schedule File Path: ${scheduleFilePath}`);
 
     const scheduleFile = await fs.readFile(scheduleFilePath, "utf-8");
 
-    core.debug(`Schedule file: ${scheduleFile}`);
+    core.notice(`Schedule file: ${scheduleFile}`);
 
     const schedule = JSON.parse(scheduleFile);
 
@@ -29,14 +44,50 @@ async function main() {
       throw new Error("No Lock Schedule Found.");
     }
 
-    core.debug(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
+    core.notice(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
 
     for (const lock of schedule.locks) {
-      if (!lock.days) {
-        throw new Error(`Missing Lock days: ${lock}`);
+      if (!lock.name) {
+        throw new Error(`Missing Lock Name: ${JSON.stringify(lock, null, 2)}`);
       }
 
-      core.notice(`Lock: ${lock}`);
+      core.notice(`Processing "${lock.name}`);
+
+      if (!lock.days) {
+        throw new Error(`Missing Lock days: ${lock.name}`);
+      }
+
+      for (const day of lock.days) {
+        if (!day) {
+          throw new Error(`Expected a day, got: "${day}`);
+        }
+
+        core.notice(`Processing ${day}`);
+
+        if (!DAYS[day.toLowerCase()]) {
+          throw new Error(
+            `Unexpected day: "${day}. Acceptable options are: ${JSON.stringify(DAYS, null, 2)}`,
+          );
+        }
+
+        let startDay = daysjs();
+        if (day.startTimeZone) {
+          startDay = startDay.tz(startTimeZone);
+        }
+
+        startDay = DAYS[startDay.getDay()];
+
+        let endDay = daysjs();
+        if (day.startTimeZone) {
+          endDay = endDay.tz(startTimeZone);
+        }
+
+        endDay = DAYS[endDay.getDay()];
+
+        if (startDay !== day && endDay !== day) {
+          core.notice(`StartDay=${startDay} EndDay=${endDay} day=${day}`);
+        }
+      }
     }
   } catch (error) {
     core.setFailed(error.message);
