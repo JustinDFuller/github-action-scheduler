6f487d8382ff228ede06ca0751980aa407802a7a
Justin Fuller <justindanielfuller@gmail.com>
2024-07-26 01:44:06 +0000

    wip

--- dist/index.js
+++ dist/index.js
@@ -26819,37 +26819,48 @@ const core = __nccwpck_require__(2186);
 // const github = require('@actions/github');
 
 async function main() {
-    try {
-        const branch = core.getInput('branch');
-        if (!branch) {
-            throw new Error(`Expected a branch to lock. Got: "${branch}"`);
-        }
-    
-        const scheduleFilePath = core.getInput('schedule-file-path');
-        if (!scheduleFilePath) {
-            throw new Error(`Expected a schedule file path. Got: "${scheduleFilePath}`);
-        }
-    
-        core.notice(`Branch: ${branch}`);
-        core.notice(`Schedule File Path: ${scheduleFilePath}`);
-    
-        const scheduleFile = await fs.readFile(scheduleFilePath, 'utf-8')
-    
-        core.notice(`Schedule file: ${scheduleFile}`);
-
-        const schedule = JSON.parse(scheduleFile);
-
-        if (!schedule || !schedule.locks || schedule.locks.length === 0) {
-            throw new Error("No Lock Schedule Found.");
-        }
-
-        core.notice(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
-    } catch (error) {
-        core.setFailed(error.message);
+  try {
+    const branch = core.getInput("branch");
+    if (!branch) {
+      throw new Error(`Expected a branch to lock. Got: "${branch}"`);
     }
+
+    const scheduleFilePath = core.getInput("schedule-file-path");
+    if (!scheduleFilePath) {
+      throw new Error(
+        `Expected a schedule file path. Got: "${scheduleFilePath}`,
+      );
+    }
+
+    core.debug(`Branch: ${branch}`);
+    core.debug(`Schedule File Path: ${scheduleFilePath}`);
+
+    const scheduleFile = await fs.readFile(scheduleFilePath, "utf-8");
+
+    core.debug(`Schedule file: ${scheduleFile}`);
+
+    const schedule = JSON.parse(scheduleFile);
+
+    if (!schedule || !schedule.locks || schedule.locks.length === 0) {
+      throw new Error("No Lock Schedule Found.");
+    }
+
+    core.debug(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
+
+    for (const lock of schedule.locks) {
+      if (!lock.days) {
+        throw new Error(`Missing Lock days: ${lock}`);
+      }
+
+      core.notice(`Lock: ${lock}`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 main();
+
 })();
 
 module.exports = __webpack_exports__;
