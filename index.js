3a93c7f0d475c50d759b21d199d8d154e2beb4f5
Justin Fuller <justindanielfuller@gmail.com>
2024-07-26 13:08:00 +0000

    test

--- index.js
+++ index.js
@@ -1,97 +1,133 @@
-const fs = require("fs/promises");
-const core = require("@actions/core");
-const dayjs = require("dayjs");
-const utc = require("dayjs/plugin/utc");
-const timezone = require("dayjs/plugin/timezone");
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-const DAYS = [
-  "sunday",
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-];
-
-async function main() {
-  try {
-    const branch = core.getInput("branch");
-    if (!branch) {
-      throw new Error(`Expected a branch to lock. Got: "${branch}"`);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-
-    const scheduleFilePath = core.getInput("schedule-file-path");
-    if (!scheduleFilePath) {
-      throw new Error(
-        `Expected a schedule file path. Got: "${scheduleFilePath}`,
-      );
-    }
-
-    core.notice(`Branch: ${branch}`);
-    core.notice(`Schedule File Path: ${scheduleFilePath}`);
-
-    const scheduleFile = await fs.readFile(scheduleFilePath, "utf-8");
-
-    core.notice(`Schedule file: ${scheduleFile}`);
-
-    const schedule = JSON.parse(scheduleFile);
-
-    if (!schedule || !schedule.locks || schedule.locks.length === 0) {
-      throw new Error("No Lock Schedule Found.");
-    }
-
-    core.notice(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
-
-    for (const lock of schedule.locks) {
-      if (!lock.name) {
-        throw new Error(`Missing Lock Name: ${JSON.stringify(lock, null, 2)}`);
-      }
-
-      core.notice(`Processing "${lock.name}`);
-
-      if (!lock.days) {
-        throw new Error(`Missing Lock days: ${lock.name}`);
-      }
-
-      for (const day of lock.days) {
-        if (!day) {
-          throw new Error(`Expected a day, got: "${day}`);
-        }
-
-        core.notice(`Processing ${day}`);
-
-        if (!DAYS.includes(day.toLowerCase())) {
-          throw new Error(
-            `Unexpected day: "${day}". Acceptable options are: ${JSON.stringify(DAYS, null, 2)}`,
-          );
-        }
-
-        let startDay = dayjs();
-        if (day.startTimeZone) {
-          startDay = startDay.tz(startTimeZone);
-        }
-
-        startDay = DAYS[startDay.day()];
-
-        let endDay = dayjs();
-        if (day.startTimeZone) {
-          endDay = endDay.tz(startTimeZone);
-        }
-
-        endDay = DAYS[endDay.day()];
-
-        if (startDay !== day && endDay !== day) {
-          core.notice(`StartDay=${startDay} EndDay=${endDay} day=${day}`);
-        }
-      }
-    }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var core = require("@actions/core");
+var dayjs = require("dayjs");
+var utc = require("dayjs/plugin/utc");
+var timezone = require("dayjs/plugin/timezone");
+var Day;
+(function (Day) {
+    Day[Day["sunday"] = 0] = "sunday";
+    Day[Day["monday"] = 1] = "monday";
+    Day[Day["tuesday"] = 2] = "tuesday";
+    Day[Day["wednesday"] = 3] = "wednesday";
+    Day[Day["thursday"] = 4] = "thursday";
+    Day[Day["friday"] = 5] = "friday";
+    Day[Day["saturday"] = 6] = "saturday";
+})(Day || (Day = {}));
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var branch, scheduleFilePath, scheduleFile, schedule, _i, _a, lock, _b, _c, day, startDate, startDay, endDate, endDay, error_1;
+        return __generator(this, function (_d) {
+            switch (_d.label) {
+                case 0:
+                    _d.trys.push([0, 2, , 3]);
+                    dayjs.extend(utc);
+                    dayjs.extend(timezone);
+                    branch = core.getInput("branch");
+                    if (!branch) {
+                        throw new Error("Expected a branch to lock. Got: \"".concat(branch, "\""));
+                    }
+                    scheduleFilePath = core.getInput("schedule-file-path");
+                    if (!scheduleFilePath) {
+                        throw new Error("Expected a schedule file path. Got: \"".concat(scheduleFilePath));
+                    }
+                    core.notice("Branch: ".concat(branch));
+                    core.notice("Schedule File Path: ".concat(scheduleFilePath));
+                    return [4 /*yield*/, fs_1.promises.readFile(scheduleFilePath, "utf-8")];
+                case 1:
+                    scheduleFile = _d.sent();
+                    core.notice("Schedule file: ".concat(scheduleFile));
+                    schedule = JSON.parse(scheduleFile);
+                    if (!schedule || !schedule.locks || schedule.locks.length === 0) {
+                        throw new Error("No Lock Schedule Found.");
+                    }
+                    core.notice("Schedules: ".concat(JSON.stringify(schedule.locks, null, 2)));
+                    for (_i = 0, _a = schedule.locks; _i < _a.length; _i++) {
+                        lock = _a[_i];
+                        if (!lock.name) {
+                            throw new Error("Missing Lock Name: ".concat(JSON.stringify(lock, null, 2)));
+                        }
+                        core.notice("Processing \"".concat(lock.name, "\""));
+                        if (!lock.days) {
+                            throw new Error("Missing Lock days: ".concat(lock.name));
+                        }
+                        for (_b = 0, _c = lock.days; _b < _c.length; _b++) {
+                            day = _c[_b];
+                            if (!day) {
+                                throw new Error("Expected a day, got: \"".concat(day));
+                            }
+                            core.notice("Processing \"".concat(lock.name, "\".\"").concat(day, "\""));
+                            if (!Day[day]) {
+                                throw new Error("Unexpected day: \"".concat(day, "\". Acceptable options are: ").concat(JSON.stringify(Day, null, 2), ". Days are case-sensitive."));
+                            }
+                            startDate = dayjs();
+                            if (lock.startTimeZone) {
+                                startDate = startDate.tz(lock.startTimeZone);
+                            }
+                            startDay = Day[startDate.day()];
+                            if (!startDay) {
+                                throw new Error("Unexpected Start Day: ".concat(startDate.day()));
+                            }
+                            endDate = dayjs();
+                            if (lock.endTimeZone) {
+                                endDate = endDate.tz(lock.endTimeZone);
+                            }
+                            endDay = Day[endDate.day()];
+                            if (!endDay) {
+                                throw new Error("Unexpected Start Day: ".concat(endDate.day()));
+                            }
+                            if (startDay !== Day[day] && endDay !== Day[day]) {
+                                core.notice("Day not matched. StartDay=".concat(startDay, " EndDay=").concat(endDay, " day=").concat(day));
+                                continue;
+                            }
+                            core.notice("Day matched. StartDay=".concat(startDay, " EndDay=").concat(endDay, " day=").concat(day));
+                        }
+                    }
+                    return [3 /*break*/, 3];
+                case 2:
+                    error_1 = _d.sent();
+                    core.setFailed(error_1.message);
+                    return [3 /*break*/, 3];
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
 }
-
 main();
---
+++ index.ts
@@ -0,0 +1,124 @@
+import { promises as fs } from "fs";
+import * as core from "@actions/core";
+import * as dayjs from "dayjs";
+import * as utc from "dayjs/plugin/utc";
+import * as timezone from "dayjs/plugin/timezone";
+
+type Schedule = {
+  readonly locks: Lock[];
+};
+
+type Lock = {
+  readonly name: string;
+  readonly days: Day[];
+  readonly startHour: number;
+  readonly startTimeZone?: string;
+  readonly endHour: number;
+  readonly endTimeZone?: string;
+};
+
+enum Day {
+  sunday,
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+}
+
+async function main() {
+  try {
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+
+    const branch = core.getInput("branch");
+    if (!branch) {
+      throw new Error(`Expected a branch to lock. Got: "${branch}"`);
+    }
+
+    const scheduleFilePath = core.getInput("schedule-file-path");
+    if (!scheduleFilePath) {
+      throw new Error(
+        `Expected a schedule file path. Got: "${scheduleFilePath}`,
+      );
+    }
+
+    core.notice(`Branch: ${branch}`);
+    core.notice(`Schedule File Path: ${scheduleFilePath}`);
+
+    const scheduleFile = await fs.readFile(scheduleFilePath, "utf-8");
+
+    core.notice(`Schedule file: ${scheduleFile}`);
+
+    const schedule: Schedule = JSON.parse(scheduleFile);
+
+    if (!schedule || !schedule.locks || schedule.locks.length === 0) {
+      throw new Error("No Lock Schedule Found.");
+    }
+
+    core.notice(`Schedules: ${JSON.stringify(schedule.locks, null, 2)}`);
+
+    for (const lock of schedule.locks) {
+      if (!lock.name) {
+        throw new Error(`Missing Lock Name: ${JSON.stringify(lock, null, 2)}`);
+      }
+
+      core.notice(`Processing "${lock.name}"`);
+
+      if (!lock.days) {
+        throw new Error(`Missing Lock days: ${lock.name}`);
+      }
+
+      for (const day of lock.days) {
+        if (!day) {
+          throw new Error(`Expected a day, got: "${day}`);
+        }
+
+        core.notice(`Processing "${lock.name}"."${day}"`);
+
+        if (!Day[day]) {
+          throw new Error(
+            `Unexpected day: "${day}". Acceptable options are: ${JSON.stringify(Day, null, 2)}. Days are case-sensitive.`,
+          );
+        }
+
+        let startDate = dayjs();
+        if (lock.startTimeZone) {
+          startDate = startDate.tz(lock.startTimeZone);
+        }
+
+        const startDay = Day[startDate.day()];
+        if (!startDay) {
+          throw new Error(`Unexpected Start Day: ${startDate.day()}`);
+        }
+
+        let endDate = dayjs();
+        if (lock.endTimeZone) {
+          endDate = endDate.tz(lock.endTimeZone);
+        }
+
+        const endDay = Day[endDate.day()];
+        if (!endDay) {
+          throw new Error(`Unexpected Start Day: ${endDate.day()}`);
+        }
+
+        if (startDay !== Day[day] && endDay !== Day[day]) {
+          core.notice(
+            `Day not matched. StartDay=${startDay} EndDay=${endDay} day=${day}`,
+          );
+
+          continue;
+        }
+
+        core.notice(
+          `Day matched. StartDay=${startDay} EndDay=${endDay} day=${day}`,
+        );
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
